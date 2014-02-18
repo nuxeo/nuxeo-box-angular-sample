8fc6b074464f9fef08a441727814efea78e8b49f
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-02-19 00:56:26 +0100

    NXIO-96: make it possible to fetch box file - add file service

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -2,7 +2,7 @@
 
 var controllerModuleApp = angular.module('boxNuxeoSampleApp.controller', ['ngResource']);
 
-controllerModuleApp.controller('NXBoxController', function ($scope, $resource, $http, cacheService, folderService, $route) {
+controllerModuleApp.controller('NXBoxController', function ($scope, $resource, $http, cacheService, folderService, $route, fileService) {
 
   //Clear errors when refreshing
   $scope.requestError = null;
@@ -45,12 +45,31 @@ controllerModuleApp.controller('NXBoxController', function ($scope, $resource, $
   $scope.fetchFolder = function (folderId) {
     return fetchFolder(folderService, $scope, folderId);
   }
+
+  // Fetch file
+  $scope.fetchFile = function (fileId) {
+    return fetchFile(fileService, $scope, fileId);
+  }
 });
 
 function fetchFolder(folderService, $scope, folderId) {
   $scope.requestError = null;
   $('#loadingWidget').show();
   folderService.get({folderId: folderId}, function (response) {
+    $scope.boxFolder = response;
+    $('#loadingWidget').hide();
+  }, function (error) {
+    $scope.requestError = error.status;
+    if (error.status === 401) {
+      $scope.clearToken();
+    }
+  });
+}
+
+function fetchFile(fileService, $scope, fileId) {
+  $scope.requestError = null;
+  $('#loadingWidget').show();
+  fileService.get({fileId: fileId}, function (response) {
     $scope.boxFolder = response;
     $('#loadingWidget').hide();
   }, function (error) {
--- app/scripts/rest_service.js
+++ app/scripts/rest_service.js
@@ -5,3 +5,7 @@ var restServiceModuleApp = angular.module('boxNuxeoSampleApp.rest_service', ['ng
 restServiceModuleApp.factory('folderService', function ($resource) {
   return $resource("https://api.box.com/2.0/folders/:folderId", {});
 });
+
+restServiceModuleApp.factory('fileService', function ($resource) {
+  return $resource("https://api.box.com/2.0/files/:fileId", {});
+});
