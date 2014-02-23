f3f1e8997a4452a4751fd5cf91eacf3399507d75
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-02-23 18:29:04 +0100

    NXIO-103: add dependencies to make karma/yasmine test work

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: NXBoxController', function () {
 
   // load the controller's module
   beforeEach(module('boxNuxeoSampleApp'));
 
-  var MainCtrl,
+  var NXBoxCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    NXBoxCtrl = $controller('NXBoxController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('scope should be defined', function () {
+    expect(scope).toBeDefined();
   });
 });
